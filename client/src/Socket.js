12be80e98a31d86a30b7aa24b5f1b7359bf4b39f
DarkoProg <marko.golob99@gmail.com>
2023-10-11 13:43:26 +0200

    changed server url

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -3,6 +3,6 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://tictactoeoncloud-o5ucqy4g4q-ew.a.run.app'; //change to https://tictactoeoncloud-o5ucqy4g4q-ew.a.run.app/:5000
 
-export const socket = io(URL, {
+export const socket = io({
     autoConnect: false,
 });
